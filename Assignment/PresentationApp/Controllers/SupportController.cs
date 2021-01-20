a74ab0a08338f5e70d63fc10599b48deeb8860a8
Beverly Cutajar <Beverly.Cutajar.c10200@mcast.edu.mt>
2021-01-20 22:12:33 +0100

    Change 1

---
+++ Assignment/PresentationApp/Controllers/SupportController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationApp.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost] //recieve data from a form submission
+        public ActionResult Contact(string email, string query)
+        {
+
+            if (string.IsNullOrEmpty(query))
+            {
+                ViewData["error"] = "One of the inputs is left empty";
+            }
+            else ViewData["feedback"] = "Thank you for your query. We will be with you shortly";
+
+            return View();
+        }
+    }
+}
