ea295f9d6c5a8cc2bea21f0ce24208cde38b94d6
Beverly Cutajar <Beverly.Cutajar.c10200@mcast.edu.mt>
2021-01-24 19:06:35 +0100

    QR Code implemented - QR not showing up

--- Assignment/PresentationApp/Startup.cs
+++ Assignment/PresentationApp/Startup.cs
@@ -29,22 +29,21 @@ namespace PresentationApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             
-
-
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-
+            
             services.AddControllersWithViews();
             services.AddRazorPages();
-
+            
             DependencyContainer.RegisterServices(services, Configuration.GetConnectionString("DefaultConnection"));
         }
-
+     
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
